8e4df75b3d6b3fed6c3440e904cdcc20eade50e1
cesslab <anwarruff@gmail.com>
2018-12-10 14:01:06 -0500

    updated session config

--- settings.py
+++ settings.py
@@ -12,12 +12,12 @@ SESSION_CONFIG_DEFAULTS = {
 }
 
 SESSION_CONFIGS = [
-    #{
-    #    'name': 'public_goods',
-    #    'display_name': "Public Goods",
-    #    'num_demo_participants': 3,
-    #    'app_sequence': ['public_goods', 'payment_info'],
-    #},
+    {
+       'name': 'continuous_choice',
+       'display_name': 'Continuous Choice',
+       'num_demo_participants': 2,
+       'app_sequence': ['game'],
+    },
 ]
 
 
