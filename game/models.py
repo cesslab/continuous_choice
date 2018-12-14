c43c1610e82f28676aeb88c640c61da6ba1bbfa0
cesslab <anwarruff@gmail.com>
2018-12-14 12:22:55 -0500

    Added games

--- game/models.py
+++ game/models.py
@@ -4,17 +4,22 @@ from otree.api import (
 )
 
 
-author = 'Your name here'
+author = 'Anwar A. Ruff'
 
 doc = """
-Your app description
+Game Play Stage
 """
 
 
 class Constants(BaseConstants):
     name_in_url = 'game'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 3
+    games = [
+        [[1, 2], [3, 4]],
+        [[5, 6], [7, 8]],
+        [[9, 10], [11, 12]],
+    ]
 
 
 class Subsession(BaseSubsession):
