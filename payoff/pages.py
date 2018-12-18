92e5910b5ee8390b0469d760d1411b7082cc3ed9
cesslab <anwarruff@gmail.com>
2018-12-18 14:51:28 -0500

    Added message page, and renamed Game page to GameOutcome

--- payoff/pages.py
+++ payoff/pages.py
@@ -3,14 +3,16 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class Game(Page):
+class GameOutcome(Page):
     def vars_for_template(self):
+        move_label = {0: 'None', 1: 'A', 2: 'B'}
         obj = self.participant.vars['payment_games'][self.round_number - 1]
         return {
             'round': obj['round'],
             'game': obj['game'],
             'payoff': obj['payoff'],
-            'move': obj['move']
+            'row_move': move_label[obj['row_move']],
+            'column_move': move_label[obj['column_move']],
         }
 
 
@@ -18,8 +20,17 @@ class FinalPayoff(Page):
     def is_displayed(self):
         return self.round_number == Constants.num_rounds
 
+    def vars_for_template(self):
+        return {'final_payoff': self.participant.payoff.to_real_world_currency(self.session)}
+
+
+class Message(Page):
+    def is_displayed(self):
+        return self.round_number == 1
+
 
 page_sequence = [
-    Game,
+    Message,
+    GameOutcome,
     FinalPayoff,
 ]
