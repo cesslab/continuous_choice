b038dba03b092a71ee348c2925f8856867eda94a
cesslab <anwarruff@gmail.com>
2018-12-18 14:50:39 -0500

    Added test for game phase

--- game/tests.py
+++ game/tests.py
@@ -7,5 +7,16 @@ from .models import Constants
 class PlayerBot(Bot):
 
     def play_round(self):
-        yield (pages.MyPage)
-        yield (pages.Results)
+        strategies = '[{"strategies":"A", "time": 4.2}, {"strategies":"B", "time": 55.807}, {"strategies": "A", "time": 58.013}]'
+        yield (pages.Game, {
+            'strategies': strategies,
+            'final_strategy': 'A',
+            'row_move': 'B'
+        })
+
+        assert (self.player.strategies == strategies)
+        assert (self.player.final_strategy == 'A')
+        assert (self.player.random_strategy == 'B')
+
+        if self.round_number == 3:
+            assert (len(self.participant.vars['payment_games']) == 2)
