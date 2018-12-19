6f9809df0417faa07c8b6913ba1c29bcf97d7f3c
cesslab <anwarruff@gmail.com>
2018-12-19 18:20:35 -0500

    Updated participation fee

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ mturk_hit_settings = {
 
 SESSION_CONFIG_DEFAULTS = {
     'real_world_currency_per_point': 0.01,
-    'participation_fee': 0.00,
+    'participation_fee': 3.00,  #HIT
     'doc': "",
     'mturk_hit_settings': mturk_hit_settings,
 }
